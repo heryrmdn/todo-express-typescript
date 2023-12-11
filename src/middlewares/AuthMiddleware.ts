cf47ef042cc395404d83fb681fe85278c40d303f
Hery Rhamadan <heryrhamadan@gmail.com>
2023-12-11 18:54:19 +0700

    refacotor: add authentication to pofile

--- src/middlewares/AuthMiddleware.ts
+++ src/middlewares/AuthMiddleware.ts
@@ -1,9 +1,24 @@
 import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
 
 export const auth = (req: Request, res: Response, next: NextFunction): any => {
-    let auth = false;
+    if (!req.headers.authorization) {
+        return res.status(401).send("ga ada token");
+    }
 
-    if (auth) return next();
+    let secretKey = process.env.JWT_SECRET_KEY || "secret";
+    const token: string = req.headers.authorization.split(" ")[1];
 
-    return res.send("unauthenticated");
+    try {
+        const credential: string | object = jwt.verify(token, secretKey);
+
+        if (credential) {
+            req.app.locals.credential = credential;
+            next();
+        }
+
+        return res.send("token invalid");
+    } catch (error) {
+        return res.send(error);
+    }
 }
